0fb8d52453867888fdb940d640fbe1da8c16b3cd
hasanisu <hasanjimam@gmail.com>
2023-11-24 13:02:09 +0900

    done students feacture

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,7 +3,8 @@ import { StudentServices } from './student.service'
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const student = req.body
+    const student = req.body.student //old way
+    // const { student: studentData } = req.body.student
     //will call service func to send this data
     const result = await StudentServices.createStudentIntoDB(student)
 
@@ -18,6 +19,39 @@ const createStudent = async (req: Request, res: Response) => {
   }
 }
 
+//Get Response
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB()
+
+    res.status(200).json({
+      sucess: true,
+      message: 'Students are retriveed successfully',
+      data: result,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//Get Data
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const studentId = req.params.studentId
+    const result = await StudentServices.getSingleStudentsFromDB(studentId)
+
+    res.status(200).json({
+      sucess: true,
+      message: 'Students are retriveed successfully',
+      data: result,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const StudentController = {
   createStudent,
+  getAllStudents,
+  getSingleStudent,
 }
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -5,3 +5,9 @@ const router = express.Router()
 
 //Will call controller function
 router.post('/create-student', StudentController.createStudent)
+router.get('/', StudentController.getAllStudents)
+router.get('/:studentId', StudentController.getSingleStudent)
+
+// eikhane router take object akare pass kora hosse na karon router call kortese
+// StudentController k r controller jehetu nijei object tai
+export const StudentRoutes = router
