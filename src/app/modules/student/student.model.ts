cc9fbb5fc751fc86e18f434a3caf46c85a77ecba
hasanisu <hasanjimam@gmail.com>
2023-11-26 15:14:12 +0900

    update student model

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -28,20 +28,36 @@ const localGuardiaSchema = new Schema<LocalGuardian>({
 //create a schema
 const studentSchema = new Schema<Student>({
   id: { type: String },
-  name: userNameSchema,
-
-  gender: ['male', 'female'],
+  name: { type: userNameSchema, required: true },
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'],
+    required: true,
+  },
   dateOfBirth: { type: String },
   email: { type: String, required: true },
   contactNo: { type: String, required: true },
   emergencyContactNo: { type: String, required: true },
-  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'],
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'],
+  },
   presentAddress: { type: String, required: true },
   permanentAdress: { type: String, required: true },
-  guardian: guardianSchema,
-  localGuardian: localGuardiaSchema,
+  guardian: {
+    type: guardianSchema,
+    required: true,
+  },
+  localGuardian: {
+    type: localGuardiaSchema,
+    required: true,
+  },
   profileImg: { type: String, required: true },
-  isActive: ['active', 'blocked'],
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 })
 
 ///Create a model
